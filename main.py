7f7435d463bf743fc369e0312f3a692b379b689c
Salah Alali <salahalalix@Salahs-MacBook-Pro.local>
2025-07-21 12:52:11 +0300

    Dynamic port for Render Deployment

--- main.py
+++ main.py
@@ -66,4 +66,6 @@ def predict(input: PredictionInput):
         raise HTTPException(status_code=500, detail=str(e))
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.environ.get("PORT", 8000)) 
+    uvicorn.run(app, host="0.0.0.0", port=port)
